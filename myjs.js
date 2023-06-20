ee377c42b7773166c6ca6fa82a3e0a47f3874f27
kyosukeedo <131092531+kyosukeedo@users.noreply.github.com>
2023-06-20 10:28:28 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,8 @@
-alert('Hello,Kyousuke!');
+alert('Hello, Kyousuke!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt(Number('2')+('3')));
+alert(Math.sqrt('2'+3));
+alert(Math.sqrt(number('2')+3));
